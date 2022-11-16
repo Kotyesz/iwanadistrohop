789ebb5ab0548aed08a01ebd60f2edebb6334bd5
Farkas Martin <47693688+Kotyesz@users.noreply.github.com>
2022-11-16 23:38:32 +0100

    Update setupscript.c

--- setupscript.c
+++ setupscript.c
@@ -14,6 +14,7 @@ int main(void){
     snprintf(input, 100, 
     "sudo pacman -Syu zsh xorg xorg-xinit alacritty bspwm sxhkd nitrogen picom chromium dmenu %s lxsession",
      drivers[0x30-input[0]]);
+    printf("%s", input);
     system(input);
 
     printf("We done bruv.\n\n");
