1c73cd54fc8444058f44cb82b7d74183ef586d31
Farkas Martin <47693688+Kotyesz@users.noreply.github.com>
2022-11-16 23:13:37 +0100

    Add files via upload

---
+++ makefile
@@ -0,0 +1,1 @@
+gcc -o run setupscript.c
---
+++ setupscript.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void){
+    char input[100], *drivers[]={
+        "xf86-video-qlx",
+        "xf86-video-amdgpu",
+        "xf86-video-intel",
+        "nvidia nvidia-utils"
+    };
+    while(input[0]<0x30||input>0x33)
+        scanf("VM(0), AMD(1), INTEL(2) or NVIDIA(3): %c", &input[0]); 
+    snprintf(input, 100, 
+    "sudo pacman -Syu xorg xorg-xinit alacritty bspwm sxhkd nitrogen picom chromium dmenu %s lxsession"
+    , drivers[0x30-input[0]]);
+    system(input);
+    return 0;
+}
