7fb8ef9ce1e892eaecb4fd9932765aad024b0931
Farkas Martin <47693688+Kotyesz@users.noreply.github.com>
2022-11-20 12:10:58 +0100

    Update setupscript.c

--- setupscript.c
+++ setupscript.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
+
+#define VM "xf86-video-qlx"
+#define AMD "xf86-video-amdgpu"
+#define INTEL "xf86-video-intel"
+#define NVIDIA "nvidia nvidia-utils"
 int main(void){
     short choice;
-    char input[1024], *drivers[]={
-        "xf86-video-qlx",
-        "xf86-video-amdgpu",
-        "xf86-video-intel",
-        "nvidia nvidia-utils"
-    };
+    char input[1024];
     while(!(choice>-1&&choice<4)){
         system("clear");
         printf("VM(0), AMD(1), INTEL(2) or NVIDIA(3): ");
@@ -14,7 +14,7 @@ int main(void){
     }
     snprintf(input, 100, 
     "sudo pacman -Syu zsh xorg xorg-xinit alacritty bspwm sxhkd nitrogen picom chromium dmenu %s lxsession",
-     drivers[choice]);
+        choice==1?AMD:choice==2?INTEL:choice==3?NVIDIA:VM);
     printf("\n%s\n\n", input);
     system("sleep 5");
     system(input);
